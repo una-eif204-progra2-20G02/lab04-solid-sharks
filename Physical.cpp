abea8736cbd97a6bec339fbaafd0c813031996fb
John Acuña Gómez <68917346+Jagtec@users.noreply.github.com>
2020-08-21 17:50:24 -0600

    Physical

---
+++ Physical.cpp
@@ -0,0 +1,5 @@
+//
+// Created by johne on 21/8/2020.
+//
+
+#include "Physical.h"
---
+++ Physical.h
@@ -0,0 +1,14 @@
+//
+// Created by johne on 21/8/2020.
+//
+
+#ifndef LAB04_SOLID_PHYSICAL_H
+#define LAB04_SOLID_PHYSICAL_H
+
+
+class Physical {
+
+};
+
+
+#endif //LAB04_SOLID_PHYSICAL_H
